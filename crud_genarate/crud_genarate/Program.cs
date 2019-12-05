100797abfccb4b16e52d1bbad45acc187a07db09
Huynh Lam Phu Si <32560385+phusidcn@users.noreply.github.com>
2019-12-05 21:20:04 +0700

    generate template
    Generate a default template

--- crud_genarate/crud_genarate/Program.cs
+++ crud_genarate/crud_genarate/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EnvDTE;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
 
 namespace crud_genarate
 {
@@ -14,9 +18,21 @@ namespace crud_genarate
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Main());
+
+            System.Type dteType = Type.GetTypeFromProgID("VisualStudio.DTE.15.0", true);
+            Object obj = System.Activator.CreateInstance(dteType, true);
+            EnvDTE.DTE dte = (EnvDTE.DTE)obj;
+            //dte.MainWindow.Visible = true;
+            dte.Solution.Create(@"C:\TemplateSolution", "TemplateSolution");
+            var solution = dte.Solution;
+
+            EnvDTE.Project project = solution.AddFromTemplate(@"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\ProjectTemplates\CSharp\Windows\1033\WindowsApplication\csWindowsApplication.vstemplate",
+                @"C:\TemplateSolution\TestTemplate", "TestTemplate");
+            dte.ExecuteCommand("File.SaveAll");
+            dte.Quit();
         }
     }
 }
--- crud_genarate/crud_genarate/ts/Model1.Designer.cs
+++ crud_genarate/crud_genarate/ts/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\KHTNPJ\Opp\opp_crud\crud_genarate\crud_genarate\ts\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Deadline-4\opp_crud\crud_genarate\crud_genarate\ts\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
